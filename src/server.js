7c1ce1ae9363b05bc9798bbcbc83bd8872ee51d9
angelolavorato <angelolavorato@hotmail.com>
2023-10-04 21:52:03 -0300

    arrumando a mensagem de controle do observador da porta

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const express = require('express') //importou o express
 const app = express() //inicializou o express
 
 const PORT = 3333 //criou uma constante que definiu o número da porta que a API vai ficar observando
-app.listen(PORT, () => console.log('Server is running on port ${PORT}'))
+app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`))
