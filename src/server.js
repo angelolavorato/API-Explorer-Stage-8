6e02d373986b7ac4ea00dc4e172e22f3bd4a5c3c
angelolavorato <angelolavorato@hotmail.com>
2023-10-04 22:11:30 -0300

    trabalhando com o método get e fazendo um 'Hello World' no local host

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express') //importou o express
 
 const app = express() //inicializou o express
 
+app.get('/', (request, response) => {
+  response.send('Hello World')
+})
+
 const PORT = 3333 //criou uma constante que definiu o número da porta que a API vai ficar observando
 app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`))
