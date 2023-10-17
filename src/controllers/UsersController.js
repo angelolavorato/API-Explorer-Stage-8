c8dad8ec8f16c1202e9ad691ed2a31ccc7c0120c
angelolavorato <angelolavorato@hotmail.com>
2023-10-17 20:32:49 -0300

    corrigindo erros

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,6 +6,12 @@ class UserController {
    * update -PUT para atualizar um registro.
    * delete - DELETE para remover um registro.
    */
+
+  create(request, response) {
+    const { name, email, password } = request.body
+
+    response.json({ name, email, password })
+  }
 }
 
 module.exports = UserController
