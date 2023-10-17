62d7bbf4103d4276c7906997c8c2b8edf1986568
angelolavorato <angelolavorato@hotmail.com>
2023-10-17 20:08:41 -0300

    algumas anotações de como o controller deve se comportar

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,11 @@
-class UserController {}
+class UserController {
+  /**
+   * index -GET para listar vários registros.
+   * show -GET para exibir um registro especifico.
+   * create -POST para criar um registro.
+   * update -PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
 
 module.exports = UserController
