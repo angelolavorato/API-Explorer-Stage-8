53c7497af7839afdfd0c6171e3b8e2e3a4c89e32
angelolavorato <angelolavorato@hotmail.com>
2023-10-17 21:28:44 -0300

    corrigindo as nomenclaturas das classes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,23 @@
-class UserController {
-  /**
-   * index -GET para listar vários registros.
-   * show -GET para exibir um registro especifico.
-   * create -POST para criar um registro.
-   * update -PUT para atualizar um registro.
-   * delete - DELETE para remover um registro.
-   */
+const AppError = require('../utils/AppError')
 
+class UserController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if (!name) {
+      throw new AppError('O nome é obrigatório')
+    }
 
     response.status(201).json({ name, email, password })
   } //testando status code
 }
 
 module.exports = UserController
+
+/** Métodos do UserControllers
+ * index -GET para listar vários registros.
+ * show -GET para exibir um registro especifico.
+ * create -POST para criar um registro.
+ * update -PUT para atualizar um registro.
+ * delete - DELETE para remover um registro.
+ */
