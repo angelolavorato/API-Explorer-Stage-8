c7925a4e3d7c21990583846c95a8f3c5412a073c
angelolavorato <angelolavorato@hotmail.com>
2023-10-17 21:45:48 -0300

    instalando o express async errors e configurando o tratamento de erros

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
-const express = require('express') //importou o express
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
+
+const express = require('express')//importou o express
 
 const routes = require('./routes')
 
-const app = express() //inicializou o express
+const app = express() //iniciou o express
 app.use(express.json())
 
 app.use(routes)
 
-const PORT = 3333 //criou uma constante que definiu o número da porta que a API vai ficar observando
-app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`))
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
