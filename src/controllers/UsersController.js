41a6a0412603e6f85f7aad9438e99923e679a7fc
angelolavorato <angelolavorato@hotmail.com>
2023-10-23 22:44:06 -0300

    corrigindo erros

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,70 +1,59 @@
 const { hash } = require('bcryptjs')
-
 const AppError = require('../utils/AppError')
-
 const sqliteConnection = require('../database/sqlite')
 
-class UserController {
+class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
     const database = await sqliteConnection()
-    const checkUserExists = await database.get(
-      'SELECT * FROM users WHERE email = (?)',
-      [email]
-    )
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if (checkUserExists) {
-      throw new AppError('Este email já está em uso')
+    if (checkUserExists){
+      throw new AppError("Este e-mail já está em uso.")
     }
 
     const hashedPassword = await hash(password, 8)
 
     await database.run(
-      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
       [name, email, hashedPassword]
     )
 
     return response.status(201).json()
   }
+
   async update(request, response) {
-    const { name, email } = request.body
-    const { id_ } = request.params
+     const { name, email } = request.body
+     const { id } = request.params
 
-    const database = await sqliteConnection()
-    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+     const database = await sqliteConnection()
+     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
-    if (!user) {
-      throw new AppError('Usuário não encontrado')
-    }
-    const userWithUpdatedEmail = await database.get(
-      'SELECT * FROM users WHERE email = (?)',
-      [email]
-    )
-    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
-      throw new AppError('Este e-mail já está em uso')
-    }
-    user.name = name
-    user.email = email
+     if(!user) {
+      throw new AppError("Usuário não encontrado")
+     }
 
-    await database.run(
-      `UPDATE users SET 
-    name = ?,
-    email =?,
-    updated_at = ? 
-    WHERE id = ?`,
+     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+     }
+
+     user.name = name
+     user.email= email
+
+     await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`, 
       [user.name, user.email, new Date(), id]
     )
-    return response.status(200).json()
+
+    return response.json()
   }
 }
 
-module.exports = UserController
-
-/** Métodos do UserControllers
- * index -GET para listar vários registros.
- * show -GET para exibir um registro especifico.
- * create -POST para criar um registro.
- * update -PUT para atualizar um registro.
- * delete - DELETE para remover um registro.
- */
+module.exports = UsersController
