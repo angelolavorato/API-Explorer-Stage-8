9c0f0a7fac487de1781111a916c45da40a317cad
angelolavorato <angelolavorato@hotmail.com>
2023-10-23 22:41:15 -0300

    fazendo o a rotina de update através do método PUT

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -27,6 +27,36 @@ class UserController {
 
     return response.status(201).json()
   }
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id_ } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado')
+    }
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
+      throw new AppError('Este e-mail já está em uso')
+    }
+    user.name = name
+    user.email = email
+
+    await database.run(
+      `UPDATE users SET 
+    name = ?,
+    email =?,
+    updated_at = ? 
+    WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+    return response.status(200).json()
+  }
 }
 
 module.exports = UserController
