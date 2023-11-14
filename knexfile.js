d9f42a2f2665de38c532050982572c952672e45a
angelolavorato <angelolavorato@hotmail.com>
2023-11-13 22:20:47 -0300

    testando as funcionalidades finais com um novo banco

--- knexfile.js
+++ knexfile.js
@@ -8,10 +8,16 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
-    }
-  },
-  migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
-  },
-  useNullAsDefault: true
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      )
+    },
+    useNullAsDefault: true
+  }
 }
