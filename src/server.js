6dfd945acdb2cfb6d2159e12fd5719575afeb9ca
angelolavorato <angelolavorato@hotmail.com>
2024-01-22 21:36:29 -0300

    instalando o cors e importando para o projeto

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 const uploadConfig = require('./configs/upload')
 
+const cors = require('cors')
 const express = require('express') //importou o express
 const routes = require('./routes')
 
 migrationsRun()
 
 const app = express() //iniciou o express
+app.use(cors())
 app.use(express.json())
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
